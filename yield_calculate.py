5ceddf2c41af86455706e7c28a60c5d997c6ae7b
BowenWang97 <BowenWang_97@outlook.com>
2024-01-26 20:29:37 +0000

    Update yield_calculate.py

--- yield_calculate.py
+++ yield_calculate.py
@@ -201,6 +201,65 @@ def yield_calculate_3d_2(parameter_1,parameter_1_delta,parameter_1_variance,para
 
     return(yield_parameter_1,yield_parameter_2,yield_parameter_3,yield_performance)
 
+def yield_calculate_3d_3(parameter_1,parameter_1_delta,parameter_1_variance,parameter_2,parameter_2_delta,parameter_2_variance,parameter_3,parameter_3_delta,parameter_3_variance,performance_1,performance_condition_1,performance_2,performance_condition_2,performance_3,performance_condition_3):
+
+    len_parameter_1 = len(parameter_1)
+    len_parameter_2 = len(parameter_2)
+    len_parameter_3 = len(parameter_3)
+
+    parameter_1_start = parameter_1[0]+2*parameter_1_variance
+    parameter_1_stop = parameter_1[len_parameter_1-1]-2*parameter_1_variance
+    parameter_2_start = parameter_2[0]+2*parameter_2_variance
+    parameter_2_stop = parameter_2[len_parameter_2-1]-2*parameter_2_variance
+    parameter_3_start = parameter_3[0]+2*parameter_3_variance
+    parameter_3_stop = parameter_3[len_parameter_3-1]-2*parameter_3_variance
+
+    len_yield_parameter_1 = int((parameter_1_stop-parameter_1_start)/parameter_1_delta)+1
+    len_yield_parameter_2 = int((parameter_2_stop-parameter_2_start)/parameter_2_delta)+1
+    len_yield_parameter_3 = int((parameter_3_stop-parameter_3_start)/parameter_3_delta)+1
+
+    number_dif_1 = int(2*parameter_1_variance/parameter_1_delta)
+    number_dif_2 = int(2*parameter_2_variance/parameter_2_delta)
+    number_dif_3 = int(2*parameter_3_variance/parameter_3_delta)
+
+    yield_parameter_1 = np.zeros(len_yield_parameter_1)
+    yield_parameter_2 = np.zeros(len_yield_parameter_2)
+    yield_parameter_3 = np.zeros(len_yield_parameter_3)
+    yield_performance = np.zeros(shape = (len_yield_parameter_1,len_yield_parameter_2,len_yield_parameter_3))
+
+    for i1 in range(len_yield_parameter_1):
+
+        yield_parameter_1[i1] = parameter_1[i1+number_dif_1]
+
+        for i2 in range(len_yield_parameter_2):
+
+            yield_parameter_2[i2] = parameter_2[i2+number_dif_2]
+
+            for i3 in range(len_yield_parameter_3):
+
+                yield_parameter_3[i3] = parameter_3[i3+number_dif_3]
+
+                yield_sum = 0
+                distribution_sum = 0
+
+                for j1 in range(2*number_dif_1+1):
+
+                    for j2 in range(2*number_dif_2+1):
+
+                        for j3 in range(2*number_dif_3):
+
+                            gaussian = math.exp(-(parameter_1[i1+j1]-yield_parameter_1[i1])*(parameter_1[i1+j1]-yield_parameter_1[i1])/(2*parameter_1_variance*parameter_1_variance)-(parameter_2[i2+j2]-yield_parameter_2[i2])*(parameter_2[i2+j2]-yield_parameter_2[i2])/(2*parameter_2_variance*parameter_2_variance)-(parameter_3[i3+j3]-yield_parameter_3[i3])*(parameter_3[i3+j3]-yield_parameter_3[i3])/(2*parameter_3_variance*parameter_3_variance))
+
+                            distribution_sum = distribution_sum+gaussian
+
+                            if (performance_1[i1+j1][i2+j2][i3+j3] >= performance_condition_1 and performance_2[i1+j1][i2+j2][i3+j3] >= performance_condition_2 and performance_condition_3[0] <= performance_3[i1+j1][i2+j2][i3+j3] <= performance_condition_3[1]):
+
+                                yield_sum = yield_sum+gaussian
+                    
+                yield_performance[i1][i2][i3] = yield_sum/distribution_sum
+
+    return(yield_parameter_1,yield_parameter_2,yield_parameter_3,yield_performance)
+
 def yield_calculate_4d(parameter_1,parameter_1_delta,parameter_1_variance,parameter_2,parameter_2_delta,parameter_2_variance,parameter_3,parameter_3_delta,parameter_3_variance,parameter_4,parameter_4_delta,parameter_4_variance,performance,performance_condition):
 
     len_parameter_1 = len(parameter_1)
