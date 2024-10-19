b6c98e24898cae42eca16d181ef3eb1e1f1289c2
BowenWang97 <BowenWang_97@outlook.com>
2024-10-19 14:32:53 +0100

    Update yield_calculate.py

--- yield_calculate.py
+++ yield_calculate.py
@@ -128,7 +128,7 @@ def yield_calculate_3d(parameter_1,parameter_1_delta,parameter_1_variance,parame
 
                     for j2 in range(2*number_dif_2+1):
 
-                        for j3 in range(2*number_dif_3+1):
+                        for j3 in range(2*number_dif_3):
 
                             gaussian = math.exp(-(parameter_1[i1+j1]-yield_parameter_1[i1])*(parameter_1[i1+j1]-yield_parameter_1[i1])/(2*parameter_1_variance*parameter_1_variance)-(parameter_2[i2+j2]-yield_parameter_2[i2])*(parameter_2[i2+j2]-yield_parameter_2[i2])/(2*parameter_2_variance*parameter_2_variance)-(parameter_3[i3+j3]-yield_parameter_3[i3])*(parameter_3[i3+j3]-yield_parameter_3[i3])/(2*parameter_3_variance*parameter_3_variance))
 
